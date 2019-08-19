dc18d003cdc0cd47002af5f49ef06f24350defe7
Ricardo Mota <ricardo.hb@outlook.com>
2019-08-19 00:49:27 -0300

    init models

---
+++ app/Models/Product.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Product extends Model implements ModelInterface
+{
+	use SoftDeletes;
+
+    protected $table = 'produto';
+    protected $dates = ['deleted_at'];
+    
+    protected $fillable = [
+        'COD_PRODUTO',
+        'DESCRICAO',
+        'COD_CATEGORIA',
+    ];
+    protected $hidden = [
+        'created_at',
+        'updated_at',
+        'deleted_at'
+    ];
+}
