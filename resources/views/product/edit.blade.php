83f1bbd96aabe675ff5e6460378597a08a02db9e
Ricardo Mota <ricardo.hb@outlook.com>
2019-08-19 02:10:08 -0300

    view to edit

---
+++ resources/views/product/edit.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="flex-center position-ref full-height container">
+        <div class="content">
+            <form method="POST" action='{{url("/update/$product->COD_PRODUTO")}}'>
+                @csrf
+                @method('PUT')
+                <div class="form-group">
+                    <label for="DESCRICAO">Descrição</label>
+                    <textarea name="DESCRICAO" class="form-control" id="DESCRICAO" rows="3">{{ $product->DESCRICAO }}</textarea>
+                </div>
+                <div class="form-group">
+                    <label for="exampleInputPassword1">Categoria</label>
+                    {{$product->COD_CATEGORIA}}
+                    <select name="COD_CATEGORIA" class="form-control">
+                        @foreach($categories as $category)
+                            @if($category->COD_CATEGORIA == $product->COD_CATEGORIA)
+                                <option selected="selected" value="{{ $category->COD_CATEGORIA }}">{{ $category->DESCRICAO }}</option>
+                            @else
+                                <option value="{{ $category->COD_CATEGORIA }}">{{ $category->DESCRICAO }}</option>
+                            @endif
+                        @endforeach
+                    </select>
+                </div>
+                <button type="submit" class="btn btn-primary">Salvar</button>
+            </form>
+        </div>
+    </div>
+@endsection
